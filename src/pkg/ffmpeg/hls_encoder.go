33b2272abdfab005b0cc501067699627199420c2
RusAD <ruslan3.8.9.24@gmail.com>
2025-11-25 00:03:05 +0300

    dbg

--- src/pkg/ffmpeg/hls_encoder.go
+++ src/pkg/ffmpeg/hls_encoder.go
@@ -30,20 +30,23 @@ func generateCommand(filepath string, res Resolution) (string, []string, error) 
 	command := "ffmpeg"
 	args := []string{"-y", "-i", filepath, "-vcodec", "copy", "-preset", "fast"}
 	sound := []string{"-map", "0:0", "-map", "0:1"}
-	resolutionTarget := []string{"-c:v:0", "copy"}
+	resolutionTarget := []string{}
 	streamMap := "v:0,a:0"
+	i := 0
 	if res.GreaterResolution(Resolution{X: 640, Y: 480}) {
 		sound = append(sound, "-map", "0:0", "-map", "0:1")
-
-		resolutionTarget = append(resolutionTarget, "-s:v:0", "640x480", "-c:v:0", "libx264", "-crf:0", "23")
+		i = 1
+		resolutionTarget = append(resolutionTarget, "-s:v:0", "640x480", "-c:v:0", "libx264", "-b:v:0", "1000k")
 		streamMap += " v:1,a:1"
 	}
 
 	if res.GreaterResolution(Resolution{X: 1920, Y: 1080}) {
+		i = 2
 		sound = append(sound, "-map", "0:0", "-map", "0:1")
-		resolutionTarget = append(resolutionTarget, "-s:v:2", "1920x1080", "-c:v:2", "libx264", "-crf:2", "23")
+		resolutionTarget = append(resolutionTarget, "-s:v:1", "1920x1080", "-c:v:1", "libx264", "-b:v:1", "4000k")
 		streamMap = streamMap + " v:2,a:2"
 	}
+	resolutionTarget = append(resolutionTarget, fmt.Sprintf("-c:v:%d", i), "copy")
 
 	args = append(args, sound...)
 	args = append(args, resolutionTarget...)
